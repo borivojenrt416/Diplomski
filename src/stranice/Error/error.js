a51b0ab7a0f62eea8b4d0f7a3040b8a6536cae12
borivojenrt416 <stojiljkovicborivoje97@gmail.com>
2020-03-14 12:36:19 +0100

    malo promene

--- src/stranice/Error/error.js
+++ src/stranice/Error/error.js
@@ -1,9 +1,18 @@
 import React,{Component} from 'react'
+import {
+    Link,
+  } from "react-router-dom";
 class Error extends Component{
 
     render(){
         return(
-        <h1>Error</h1>
+            <div>
+            <div className="prazno">
+              <p>Tražena stranica ne postoji!</p>
+              <p><Link className="back" to="/home">Vrati se na pocetnu stranu</Link></p>
+            </div>
+
+          </div>
         );
     }
 }
